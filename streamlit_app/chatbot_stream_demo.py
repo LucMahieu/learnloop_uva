b635d7efa7749e5539738c5f9aa7d0dab7f5d141
Luc Mahieu <lucmauricemahieu@gmail.com>
2024-06-04 18:49:02 +0200

    added probleemstelling prompt and updated interface

--- streamlit_app/chatbot_stream_demo.py
+++ streamlit_app/chatbot_stream_demo.py
@@ -6,9 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-st.write(f"OPENAI_API_KEY: {os.getenv('OPENAI_API_KEY')}")
-st.write(f"AZURE_OPENAI_ENDPOINT: {os.getenv('AZURE_OPENAI_ENDPOINT')}")
-
 # def connect_to_openai():
 #     return AzureOpenAI(
 #     api_key=os.getenv("OPENAI_API_KEY"),  
@@ -23,7 +20,7 @@ def connect_to_openai():
 
 client = connect_to_openai()
 
-st.title("ChatGPT-like clone")
+st.title("Probleemstelling bepalen")
 
 # if "openai_model" not in st.session_state:
 #     st.session_state["openai_model"] = "learnloop"
@@ -35,22 +32,27 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    with st.chat_message(message["role"], avatar='🔵' if message["role"] == "assistant" else '🔘'):
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(f"You balou: {prompt}")
+    with st.chat_message("user", avatar='🔘'):
+        st.markdown(f"{prompt}")
 
-    with st.chat_message("assistant"):
+
+    with st.chat_message("assistant", avatar='🔵'):
+        role_prompt = open("probleemstelling_prompt.txt").read()
         stream = client.chat.completions.create(
             model=st.session_state.openai_model,
             messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
+                {"role": "system", "content": role_prompt},
+                *({"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages)
             ],
             stream=True,
         )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+
